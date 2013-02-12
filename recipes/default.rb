e5df356e4ec89b995005b35fdd42117a9c8d4ea9
Tim Stumbaugh <stum@getabine.com>
2013-02-12 18:20:49 -0500

    Use the latest uWSGI instead of whatever's in the repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,9 +14,21 @@ node[:app][:provider] = 'app_flask'
 package 'uwsgi'
 package 'uwsgi-plugin-python'
 
+# Install the real uwsgi
+python_pip 'uwsgi' do
+  action :install
+end
+execute 'service uwsgi stop'
+file '/usr/bin/uwsgi' do
+  action :delete
+end
+link '/usr/bin/uwsgi' do
+  to '/usr/local/bin/uwsgi'
+end
+
 service 'uwsgi' do
   supports :restart => true, :reload => true, :status => false
-  action :enable
+  action [:enable, :start]
 end
 
 node[:app][:destination] = node[:repo][:default][:destination]
